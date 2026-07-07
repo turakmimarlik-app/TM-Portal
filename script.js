e9a7e666a88fc61d763c7959d48d07005bac84f0
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-07 21:36:14 +0300

    V1.17.0 - Nakit Dekont, Is Muhasebesi Olustur/Takibi sayfalari HTS konseptine tasarim guncellemesi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.16.0';
+        var APP_VERSION = 'V1.17.0';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
