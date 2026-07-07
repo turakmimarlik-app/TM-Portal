34fd0068d3849871e351391d2c35fe7c4daba604
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-07 03:38:11 +0300

    V1.14.1 - TL sembolu boyutu buyutuldu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.14.0';
+        var APP_VERSION = 'V1.14.1';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
