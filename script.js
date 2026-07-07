981b6efcc48576031ee46e0670568b03322d8f1f
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-07 03:00:37 +0300

    V1.10.4 - ht-kart-3d width 100% eklendi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.10.3';
+        var APP_VERSION = 'V1.10.4';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
