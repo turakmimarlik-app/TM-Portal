3e578c1192f6a35f4119c6fec396074c735e2277
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-08 00:29:45 +0300

    V1.19.1 - renk paleti piyasa fiyatlari temasiyla uyumlu hale getirildi (hts dark navy -> default light)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.19.0';
+        var APP_VERSION = 'V1.19.1';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
