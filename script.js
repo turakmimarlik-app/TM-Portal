592fadd258261bbba1d9c01658a7cd8113c13a91
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-11 02:59:36 +0300

    V1.31.15 - versiyon senkronizasyonu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.31.14';
+        var APP_VERSION = 'V1.31.15';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
