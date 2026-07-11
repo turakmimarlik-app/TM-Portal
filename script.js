c024c7e1a7a3684ea944805f0aa6e22c19d5c989
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-11 22:56:45 +0300

    V1.34.4 - tatil etiketi 12px inline, hucre 78px, gun 16px

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.34.3';
+        var APP_VERSION = 'V1.34.4';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; console.error=function(){};
