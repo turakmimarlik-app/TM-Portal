525f2d2c4e0a097533af76a48aab02535355dd17
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-14 01:26:37 +0300

    V1.26.0 - Ortaklik Statusu alani genisletildi, diger kolonlar daraltildi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.25.0';
+        var APP_VERSION = 'V1.26.0';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
