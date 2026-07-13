a4aa22cb1438cb72ad7895b2a4e9a58ff561a819
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-14 01:24:10 +0300

    V1.25.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.24.0';
+        var APP_VERSION = 'V1.25.0';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
