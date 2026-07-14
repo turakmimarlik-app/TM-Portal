91a4f9fce1a842e9ec0cb8f611196f39fd10d1da
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-14 22:51:04 +0300

    V1.32.18 portfoy ikon amber, dropdown oklar ana renk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.32.17';
+        var APP_VERSION = 'V1.32.18';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
