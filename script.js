dabedf9d333f7b206f6dbb156f49a2b2da559ef4
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-14 21:37:31 +0300

    V1.32.9 version duzeltme

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.32.8';
+        var APP_VERSION = 'V1.32.9';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
