c1782b819ef7cad2c2d64030c24d5559667cf755
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-14 22:41:30 +0300

    V1.32.15 menu isim guncellemeleri

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var APP_VERSION = 'V1.32.14';
+        var APP_VERSION = 'V1.32.15';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
