75db6d99875b280d59a78b8715f4b934997780c0
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-21 01:59:15 +0300

    V1.82.2 versiyon numarasi duzeltmesi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.82.1';
+﻿        var APP_VERSION = 'V1.82.2';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
