60d3f3b5ff7f5ff04eadd03a98e32d929c573034
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-21 02:21:08 +0300

    V1.84.2 versiyon artirimi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.84.1';
+﻿        var APP_VERSION = 'V1.84.2';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
