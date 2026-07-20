60e625f8d5b46f9ee9cd267fc92d85615684361a
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-20 22:35:04 +0300

    V1.75.0 - version güncellemesi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.74.10';
+﻿        var APP_VERSION = 'V1.75.0';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
