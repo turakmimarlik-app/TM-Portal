5018eb8f3f1a2bae139942a270a2e7f7dea40553
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-21 22:39:18 +0300

    Revert "Versiyon V1.86.2 güncellendi"
    This reverts commit 6b2ebc4155b113561dd7eaf7ea8223e142fe7272.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.86.2';
+﻿        var APP_VERSION = 'V1.86.1';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
