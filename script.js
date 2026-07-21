0d805811735ed6f3f0b2bfa0fc460de2d8384df6
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-22 02:14:20 +0300

    feat: Finans kartlarına ikon eklendi (V1.93.3)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.93.1';
+﻿        var APP_VERSION = 'V1.93.3';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
