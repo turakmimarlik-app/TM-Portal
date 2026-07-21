8b9ab7ba872638f90959f9cf0633bb9d7f9ab6f2
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-21 22:37:21 +0300

    V1.86.4 - focus anında autocomplete engelleme (en agresif yöntem)
    - Her input odaklandığı an autocomplete='nope' atanır (focus capture)
    - MutationObserver + başlangıç taraması ile tüm input'lar korunur

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.86.3';
+﻿        var APP_VERSION = 'V1.86.4';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
