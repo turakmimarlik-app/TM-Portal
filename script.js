4229af31bd59f7fee04e70e48a64a4c19128be1a
turakmimarlik-app <turakmimarlik-app@users.noreply.github.com>
2026-07-25 21:12:14 +0000

    deploy: dafe7863652aa74142ed231b870d92a702a37a9f

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.51.0';
+﻿        var APP_VERSION = 'V1.39.5';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
