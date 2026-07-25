e59e9aade646a101af3361040b8ae2bab13b469c
turakmimarlik-app <turakmimarlik@gmail.com>
2026-07-25 23:15:14 +0300

    APP_VERSION ve script.js v parametresi V1.45.0 yapildi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿        var APP_VERSION = 'V1.39.5';
+﻿        var APP_VERSION = 'V1.45.0';
 
         /* Production - console loglari kapat */
         console.log=function(){}; console.warn=function(){}; // console.error acik tutuluyor (debug)
